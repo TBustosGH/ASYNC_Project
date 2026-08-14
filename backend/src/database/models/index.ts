d9d31de357bc473d192d0cea9eda90220f3e2534
TBustosGH <tnbustos2007@gmail.com>
2026-08-14 11:20:53 -0300

    Fix: Error with sequelize models relationships ---backend--- -Fix an error where the reationships were bad specified, like "follower.hasMany(user)", which was trying to create a new "follower_id" column in Users table, and was causing troubles when selecting all data from users cuase no such column exists. The same passed with savedPosts and posts (savedPost.hasMany(posts))

--- backend/src/database/models/index.ts
+++ backend/src/database/models/index.ts
@@ -4,21 +4,22 @@ import Comment from "./models/comments.js";
 import SavedPost from "./models/savedPosts.js";
 import Follower from "./models/followers.js";
 
+
 // User model relationships
 User.hasMany(Post);
 User.hasMany(Comment);
 User.hasMany(SavedPost);
 // Post model relationships
-Post.hasMany(Comment);
 Post.belongsTo(User);
+Post.hasMany(Comment);
 // Comment model relationships
 Comment.belongsTo(Post);
 Comment.belongsTo(User);
 // SavedPost model relationships
 SavedPost.belongsTo(User);
-SavedPost.hasOne(Post)
+SavedPost.belongsTo(Post)
 // Follower model relationships
-Follower.hasMany(User);
+Follower.belongsTo(User);
 
 export default {
     User,
